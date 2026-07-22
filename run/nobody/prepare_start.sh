82dc06f8e4032bea74ec04e59da13c02b2236866
Mythandar <ennok.t@gmail.com>
2026-07-22 09:49:29 -0600

    Improve FS25 container reliability

--- run/nobody/prepare_start.sh
+++ run/nobody/prepare_start.sh
@@ -1,9 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export USER="${USER:-nobody}"
 
 . /usr/local/bin/wine_init.sh
 
 . /usr/local/bin/wine_symlinks.sh
 
 . /usr/local/bin/copy_server_config.sh
+
+/usr/local/bin/validate_fs25.sh runtime
 
 . /usr/local/bin/cleanup_logs.sh
